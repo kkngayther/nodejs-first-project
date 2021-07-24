c2935c7c2fcb5fea07e16079a3956f2e4fe83da2
18521536-LeAnhTrieu <18521536@gm.uit.edu.vn>
2021-07-24 20:43:59 +0700

    second commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// http logger
+app.use(morgan('combined'));
+
+// template engine
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// console.log(__dirname)
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.get('/news', (req, res) => {
+    res.render('news')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
